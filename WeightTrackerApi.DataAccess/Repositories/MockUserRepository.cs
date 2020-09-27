57269c38cee4c405392c3f1707fae98fdb1cc1cb
= <=>
2020-09-27 15:18:54 -0500

    implemented mock user repo

---
+++ WeightTrackerApi.DataAccess/Repositories/MockUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeightTrackerApi.Domain.Models;
+
+namespace WeightTrackerApi.DataAccess.Repositories
+{
+    public class MockUserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new List<User>();
+
+        public async Task AddUserAsync(User user)
+        {
+            await new Task(() => _users.Add(user));
+        }
+
+        public async Task DeleteUserAsync(string username)
+        {
+            var user = await GetUserAsync(username);
+
+            await new Task(() => _users.Remove(user));
+        }
+
+        public async Task<User> GetUserAsync(string username)
+        {
+
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync(string username)
+        {
+            return await new Task<IEnumerable<User>>(() => _users);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            var userInList = await GetUserAsync(user.Username);
+
+            userInList = user;
+        }
+    }
+}
