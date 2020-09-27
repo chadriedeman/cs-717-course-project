541a2db61591aba5bf08a148f5abe1b1ab35e378
= <=>
2020-09-27 11:28:54 -0500

    added validator

---
+++ WeightTrackerApi/Validators/UserDtoValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeightTrackerApi.Validators
+{
+    public class UserDtoValidator
+    {
+    }
+}
