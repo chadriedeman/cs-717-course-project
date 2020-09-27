169586a89ae55f7341d2b5f88211b31422bdbb93
= <=>
2020-09-27 14:50:21 -0500

    added props to user

--- WeightTrackerApi.Domain/Models/User.cs
+++ WeightTrackerApi.Domain/Models/User.cs
@@ -1,7 +1,12 @@
-﻿namespace WeightTrackerApi.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace WeightTrackerApi.Domain.Models
 {
     public class User
     {
         public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<WeighIn> WeighIns { get; set; }
     }
 }
