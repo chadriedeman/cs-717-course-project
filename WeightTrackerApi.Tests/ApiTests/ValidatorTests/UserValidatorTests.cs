7c0ac0f15f53552dd7051c4665c67fa39c3145bc
= <=>
2020-09-27 13:58:53 -0500

    added tests

---
+++ WeightTrackerApi.Tests/ApiTests/ValidatorTests/UserValidatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeightTrackerApi.Tests.ApiTests.ValidatorTests
+{
+    public class UserValidatorTests
+    {
+    }
+}
---
+++ WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using WeightTrackerApi.Business.Services;
+
+namespace WeightTrackerApi.Tests.BusinessTests.ServiceTests
+{
+    public class UserServiceTests
+    {
+        private readonly UserService _subjectUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            // _subjectUnderTest = new UserService();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
