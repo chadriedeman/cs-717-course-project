84dc16aa92386869280d3f96cc211a59c929ac73
= <=>
2020-10-05 05:25:32 -0500

    implemeted tests for get user

--- WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/GetUsersTests.cs
+++ WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/GetUsersTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
 
 namespace WeightTrackerApi.Tests.BusinessTests.ServiceTests.UserServiceTests
 {
-    class GetUsersTests
+    public class GetUsersTests : UserServiceTestsBase
     {
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUsernameIsEmpty()
+        {
+            var emptyUsername = string.Empty;
+
+            Action getUserCall = () => _subjectUnderTest.GetUser(emptyUsername);
+
+            getUserCall.Should().Throw<ArgumentException>()
+                .WithMessage("No username was provided to GetUser.");
+        }
+
+        [Test]
+        public void ShouldGetUserWhenUsernameIsValid()
+        {
+            var username = _chance.Sentence();
+
+            _subjectUnderTest.GetUser(username);
+
+            _userRepository.Received(1).GetUser(Arg.Any<string>());
+        }
     }
 }
