8e17a228f1688d7313878ad28665fd0b6f59c979
= <=>
2020-10-05 05:49:16 -0500

    added more tests

--- WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/UpdateUserTests.cs
+++ WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/UpdateUserTests.cs
@@ -1,10 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using WeightTrackerApi.Domain.Models;
 
 namespace WeightTrackerApi.Tests.BusinessTests.ServiceTests.UserServiceTests
 {
-    class UpdateUserTests
+    public class UpdateUserTests : UserServiceTestsBase
     {
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUserIsNull()
+        {
+            User nullUser = null;
+
+            Action updateUserCall = () => _subjectUnderTest.UpdateUser(nullUser);
+
+            updateUserCall.Should().Throw<ArgumentException>()
+                .WithMessage("No username was provided to UpdateUser.");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUsernameIsEmpty()
+        {
+            var userWithInvalidUsername = new User
+            {
+                Username = string.Empty
+            };
+
+            Action updateUserCall = () => _subjectUnderTest.UpdateUser(userWithInvalidUsername);
+
+            updateUserCall.Should().Throw<ArgumentException>()
+                .WithMessage("No username was provided to UpdateUser.");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUserDoesNotExist()
+        {
+            var user = GetValidUser();
+
+            User userInDatabase = null;
+
+            _userRepository.GetUser(Arg.Any<string>()).Returns(userInDatabase);
+
+            Action updateUserCall = () => _subjectUnderTest.UpdateUser(user);
+
+            updateUserCall.Should().Throw<ArgumentException>()
+                .WithMessage($"{user.Username} does not exist in the database.");
+        }
+
+        [Test]
+        public void ShouldUpdateUserWhenGivenValidUser()
+        {
+            var user = GetValidUser();
+
+            var userInDatabase = GetValidUser();
+
+            _userRepository.GetUser(Arg.Any<string>()).Returns(userInDatabase);
+
+            _subjectUnderTest.UpdateUser(user);
+
+            _userRepository.Received(1).UpdateUser(Arg.Any<User>());
+        }
     }
 }
