ae4efd4317ed6fbea85eeb279db6ba9df63c53cf
= <=>
2020-10-05 05:41:02 -0500

    added more tests

--- WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/GetUserWeighInTests.cs
+++ WeightTrackerApi.Tests/BusinessTests/ServiceTests/UserServiceTests/GetUserWeighInTests.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using WeightTrackerApi.Domain.Models;
 
 namespace WeightTrackerApi.Tests.BusinessTests.ServiceTests.UserServiceTests
 {
-    class GetUserWeighInTests
+    public class GetUserWeighInTests : UserServiceTestsBase
     {
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUsernameIsEmpty()
+        {
+            var emptyUsername = string.Empty;
+
+            var date = _chance.Date();
+
+            Action getUserWeighInCall = () => _subjectUnderTest.GetUserWeighIn(emptyUsername, date);
+
+            getUserWeighInCall.Should().Throw<ArgumentException>()
+                .WithMessage("No username was provided to GetUserWeighIn.");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenUserDoesNotExist()
+        {
+            var username = _chance.Sentence();
+
+            var date = _chance.Date();
+
+            User userInDatabase = null;
+
+            _userRepository.GetUser(Arg.Any<string>()).Returns(userInDatabase);
+
+            Action getUserWeighInCall = () => _subjectUnderTest.GetUserWeighIn(username, date);
+
+            getUserWeighInCall.Should().Throw<ArgumentException>()
+                .WithMessage($"{username} does not exist in the database.");
+        }
+
+        [Test]
+        public void ShouldGetUserWeighInWhenUsernameIsValid()
+        {
+            var username = _chance.Sentence();
+
+            var date = _chance.Date();
+
+            var userInDatabase = GetValidUser();
+
+            _userRepository.GetUser(Arg.Any<string>()).Returns(userInDatabase);
+
+            _subjectUnderTest.GetUserWeighIn(username, date);
+
+            _userRepository.Received(1).GetUserWeighIn(Arg.Any<string>(), Arg.Any<DateTime>());
+        }
     }
 }
